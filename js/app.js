30a432331c375c13e3bec4987ede84dc4445ca77
josetvasco <torovascojosedaniel@gmail.com>
2024-06-11 20:37:43 -0500

    Creación de alerta en caso de que todos los campos no esten llenos

--- js/app.js
+++ js/app.js
@@ -33,7 +33,40 @@ function datosCita(e) {
 function submitCita(e) {
   e.preventDefault();
   if(Object.values(citaObj).some( valor => valor.trim() === '')) {
-    console.log('Todos los campos son pbligatorios');
+    new Notificacion({
+      texto: 'Todos los campos son obligatorios',
+      tipo: 'error'
+    })
     return;
   }
+}
+
+
+class Notificacion {
+  constructor({ texto, tipo }) {
+    this.texto = texto;
+    this.tipo = tipo;
+    
+    
+    this.mostrar();
+  }
+
+  mostrar() {
+    const alerta = document.createElement('DIV');
+    alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm')
+
+    // Si es tipo derror, agrega una clase
+    this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+    // Mensaje de error
+    alerta.textContent = this.texto;
+
+    formulario.parentElement.insertBefore(alerta, formulario);
+
+    setTimeout(() => {
+      alerta.remove();
+    }, 5000);
+  }
+
+
 }
